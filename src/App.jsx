eb48305e8a4a5068f625458835542bc1c5dfbeab
Bloo72 <timjoo432@gmail.com>
2025-04-06 12:45:57 -0700

    Fix component import paths to match project directory structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
+import Sidebar from './Components/Sidebar';
 import CreateToken from './Components/CreateToken';
 import NewTokens from './Components/NewTokens';
 import TokenList from './Components/TokenList';
